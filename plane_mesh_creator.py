b02f9bfdfd915039bd3589a0439acdff7ba9aebd
Sambhav Satija <sambhav13085@iiitd.ac.in>
2016-04-13 01:35:51 +0530

    Added mesh creator script

---
+++ plane_mesh_creator.py
@@ -0,0 +1,80 @@
+from collections import namedtuple
+from math import log10
+
+Point = namedtuple('Point', ['x', 'y', 'z'])
+Plane = namedtuple('Plane', ['A', 'B', 'C', 'D'])
+Square = namedtuple('Square', ['A', 'B', 'C', 'D'])
+
+SQUARES_PER_UNIT = 10
+"""
+
+       A _______________________ B
+        /                      /
+       /                      /
+      /                      /
+     /                      /
+    /                      /
+   /______________________/
+  D                       C
+"""
+round_amount = int(log10(SQUARES_PER_UNIT))
+step = 1.0/SQUARES_PER_UNIT
+
+def float_stepper(from_, to_, step_):
+    while from_ <= to_:
+        from_ = round(from_, round_amount)
+        yield from_
+        from_ += step_
+
+plane = Plane(A = Point(-3, 0, -3),    # A
+              B = Point(3, 0, -3),     # B
+              C = Point(3, 0, 3),      # C
+              D = Point(-3, 0, 3),     # D
+)
+
+slices_x = [plane.A.x]
+squares = []
+for slice_x in float_stepper(plane.A.x + step, plane.B.x + step, step):
+    slices_x.append(slice_x)
+    slices_z = [plane.A.z]
+    for slice_z in float_stepper(plane.A.z + step, plane.D.z + step, step):
+        slices_z.append(slice_z)
+        squares.append(Square(
+            A=Point(slices_x[-2], 0, slices_z[-2]),
+            B=Point(slices_x[-1], 0, slices_z[-2]),
+            C=Point(slices_x[-1], 0, slices_z[-1]),
+            D=Point(slices_x[-2], 0, slices_z[-1]),
+            ))
+
+vertices = []
+vertices_set = set()
+vertices_find = {}
+
+faces = []
+
+for square in squares:
+    for point in (square.A, square.B, square.C, square.D):
+        if point not in vertices_set:
+            vertices.append(point)
+            vertices_set.add(point)
+            vertices_find[point] = len(vertices) - 1
+
+    faces.append((
+        vertices_find[square.A],
+        vertices_find[square.B],
+        vertices_find[square.C]
+        ))
+    faces.append((
+        vertices_find[square.A],
+        vertices_find[square.D],
+        vertices_find[square.C]
+        ))
+
+with open('plane.obj', 'w') as f:
+    for vertex in vertices:
+        f.write("v %f %f %f\n" % (vertex.x, vertex.y, vertex.z))
+
+    f.write("vt 0 0\n")
+
+    for face in faces:
+        f.write("f %d/0 %d/0 %d/0\n" % face)
